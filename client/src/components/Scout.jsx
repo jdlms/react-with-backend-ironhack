f2abd014317e4ce6536a2488d43b0aab0d8df015
jdlms <97018563+jdlms@users.noreply.github.com>
2022-11-21 18:37:42 +0100

    basic book form routing

--- client/src/components/Scout.jsx
+++ client/src/components/Scout.jsx
@@ -15,21 +15,21 @@ export function Scout() {
   const saveBookToDb = async () => {
     try {
       const formBody = bookFormState;
+      console.log(formBody);
 
-      const res = await fetch({
+      const res = await fetch("http://localhost:5005/post", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        credentials: "include",
         body: JSON.stringify(formBody),
       });
 
       const sentData = await res.json();
+      console.log("hello again");
       //
       console.log(sentData);
       //
-      setBookFormState(emptyBookForm);
     } catch (error) {
       console.error(error);
     }
@@ -38,27 +38,26 @@ export function Scout() {
   return (
     <div>
       <h1>Scout's page</h1>
-      <form action="POST">
-        <fieldset>
-          <legend>Enter book</legend>
-          <input
-            name="title"
-            onChange={handleChange}
-            value={bookFormState.title}
-            placeholder="Title"
-          />
-          <br />
-          <input
-            type="text"
-            name="author"
-            onChange={handleChange}
-            value={bookFormState.author}
-            placeholder="Author"
-          />
-          <br />
-          <button onClick={saveBookToDb}>Submit</button>
-        </fieldset>
-      </form>
+
+      <fieldset>
+        <legend>Enter book</legend>
+        <input
+          name="title"
+          onChange={handleChange}
+          value={bookFormState.title}
+          placeholder="Title"
+        />
+        <br />
+        <input
+          type="text"
+          name="author"
+          onChange={handleChange}
+          value={bookFormState.author}
+          placeholder="Author"
+        />
+        <br />
+        <button onClick={saveBookToDb}>Submit</button>
+      </fieldset>
     </div>
   );
 }
--- server/router.js
+++ server/router.js
@@ -1,17 +1,24 @@
-
-import { Router } from 'express';
-import { User } from './User.model.js';
-import axios from 'axios';
-
+import { Router } from "express";
+import { Title } from "./Title.model.js";
+import axios from "axios";
 
 const router = Router();
 
-
-
-router.get('/', (req, res) => {
-  res.json({ message: 'Hello world' });
+router.post("/post", async (req, res) => {
+  try {
+    const newTitle = new Title({
+      title: req.body.title,
+      author: req.body.author,
+    });
+    await newTitle.save();
+    return res.json({ message: "Title added!" });
+  } catch (error) {
+    console.log("There was an error", error);
+  }
 });
 
-
+router.get("/", (req, res) => {
+  res.json({ message: "Hello world" });
+});
 
 export default router;
